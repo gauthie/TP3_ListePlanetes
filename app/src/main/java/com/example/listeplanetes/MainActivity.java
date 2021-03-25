ac5a8ec9384bb600e01067a11a1bc05125b4156d
me <roussel.gauthier@gmail.com>
2021-03-25 16:14:05 +0100

    check button

--- app/src/main/java/com/example/listeplanetes/MainActivity.java
+++ app/src/main/java/com/example/listeplanetes/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
@@ -28,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listitem);
+        Button btnCheck = (Button) findViewById(R.id.btnCheck);
+        btnCheck.setOnClickListener(btnCkeckOnClickListener);
         listview = (ListView) findViewById(R.id.listView);
         adapter = new PlaneteAdapter(this, data);
         listview.setAdapter(adapter);
     }
+
+    View.OnClickListener btnCkeckOnClickListener = new View.OnClickListener(){
+        public void onClick(View v){
+
+        }
+    };
 
 }
